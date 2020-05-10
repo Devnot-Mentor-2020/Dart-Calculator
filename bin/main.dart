17dff401d930e00a9092fc076fce7b7acb5b7355
Alperen Ekin <alperenekin97@gmail.com>
2020-05-11 00:16:09 +0300

    Empty

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,4 @@
 import '../lib/CalculatorApp.dart' as CalculatorApp;
-import 'Calculator.dart';
-
 void main(List<String> arguments) {
-  var calculator = new Calculator(20,10);
-  calculator.AddFirstNumberToSecondNumber();
-  var calculator2= new Calculator(15, 25);
-  calculator2.AddFirstNumberToSecondNumber();
-  print(calculator2-calculator2);
-  print('Hello world: ${CalculatorApp.calculate()}!');
+  
 }
